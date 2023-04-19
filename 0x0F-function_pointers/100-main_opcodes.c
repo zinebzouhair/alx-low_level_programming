9cf4d8bee5759b1a9fb9b11f866411736920ff84
zinebzouhair <zinebzouhair94@gmail.com>
2023-04-19 16:52:58 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,26 +1,49 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
-* main - prints the opcodes of its own main function
-* @argc: argument count
-* @argv: argument vector
-*
-* Return: The job has been finished
-*/
-
-int main(int argc, char *argv[])
+ * print_opcodes - print the opcodes of this program
+ * @a: address of the main function
+ * @n: number of bytes to print
+ *
+ * Return: void
+ */
+void print_opcodes(char *a, int n)
 {
-	if (argc != 2)
-	{
-		printf("Error\n");
-		exit(1);
-	}
+int i;
 
-	if (atoi(argv[1]) < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
-	return (0);
+for (i = 0; i < n; i++)
+{
+printf("%.2hhx", a[i]);
+if (i < n - 1)
+printf(" ");
+}
+printf("\n");
+
+}
+
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: O
+ */
+int main(int argc, char **argv)
+{
+int a;
+
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
+}
+a = atoi(argv[1]);
+if (a < 0)
+{
+printf("Error\n");
+exit(2);
+}
+print_opcodes((char *)&main, a);
+return (0);
 }
